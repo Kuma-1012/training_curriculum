b25c75783eec26d2992ccfaac507e9cda2656de7
Kuma-1012 <yutakuma121017@icloud.com>
2021-05-25 18:16:40 +0900

    Issue4~6修正②

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    get_week
+    getWeek
     @plan = Plan.new
   end
 
@@ -15,10 +15,10 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:plan, :date, :id, :created_at, :updated_at)
+    params.require(:plan).permit(:plan, :date, :id, :created_at, :update_at)
   end
 
-  def get_week
+  def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -31,12 +31,14 @@ class CalendarsController < ApplicationController
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
-        wday_num = @todays_date.wday
-        if wday_num >= 7
-          wday_num = wday_num -7
-        end 
-        days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[(@todays_date + x).wday]}
       end
+      wday_num = @todays_date.wday
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end 
+      # wday = Date.today.wday 
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[(@todays_date + x).wday]}
+      # puts wdays[0],[1],[2],[3],[4],[5],[6]
       @week_days.push(days)
     end
   end
